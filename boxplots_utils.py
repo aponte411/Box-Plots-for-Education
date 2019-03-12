36d89d7108e85d13862a6d9537a6ef083ad09c17
David Aponte <36382674+aponte411@users.noreply.github.com>
2019-03-12 17:09:39 -0400

    Add files via upload
    Utility functions and classes

---
+++ boxplots_utils.py
@@ -0,0 +1,229 @@
+"""
+This script contains the helper functions or classes used to create the pipeline.
+Most of the functions come from pjbull and Datacamp. 
+
+https://github.com/drivendataorg/box-plots-sklearn/tree/master/src
+
+"""
+import pandas as pd
+import numpy as np 
+# indices for each label
+BOX_PLOTS_COLUMN_INDICES = [list(range(37)),
+                            list(range(37, 48)),
+                            list(range(48, 51)),
+                            list(range(51, 76)),
+                            list(range(76, 79)),
+                            list(range(79, 82)),
+                            list(range(82, 87)),
+                            list(range(87, 96)),
+                            list(range(96, 104))]
+
+
+def multi_multi_log_loss(predicted,
+                          actual,
+                          class_column_indices=BOX_PLOTS_COLUMN_INDICES,
+                          eps=1e-15):
+    """ Multi class version of Logarithmic Loss metric as implemented on
+        DrivenData.org
+    """
+    class_scores = np.ones(len(class_column_indices), dtype=np.float64)
+
+    # calculate log loss for each set of columns that belong to a class
+    for k, this_class_indices in enumerate(class_column_indices):
+        # get just the columns for this class
+        preds_k = predicted[:, this_class_indices].astype(np.float64)
+
+        # normalize so probabilities sum to one (unless sum is zero, then we clip)
+        preds_k /= np.clip(preds_k.sum(axis=1).reshape(-1, 1), eps, np.inf)
+
+        actual_k = actual[:, this_class_indices]
+
+        # shrink predictions so
+        y_hats = np.clip(preds_k, eps, 1 - eps)
+        sum_logs = np.sum(actual_k * np.log(y_hats))
+        class_scores[k] = (-1.0 / actual.shape[0]) * sum_logs
+
+    return np.average(class_scores)
+
+
+
+NUMERIC_COLS = ["FTE", "Total"]
+LABELS = ['Function',
+ 'Use',
+ 'Sharing',
+ 'Reporting',
+ 'Student_Type',
+ 'Position_Type',
+ 'Object_Type',
+ 'Pre_K',
+ 'Operating_Status']
+
+# function to combine text
+def combine_text_columns(dataframe, to_drop=NUMERIC_COLS + LABELS):
+    """ 
+    Converts all text in each row into a single vector.
+    
+    params:
+    dataframe = dataframe read in using pandas
+    to_drop = all non-text columns and labels (optional)
+    
+    """
+    
+    # drop non-text columns and labels
+    drop = set(to_drop) & set(dataframe.columns.tolist())
+    text_data = dataframe.drop(drop, axis=1)
+    
+    # replace missing values with blank space
+    text_data = text_data.fillna('')
+    
+    # join all text per row using a space
+    return text_data.apply(lambda x: " ".join(x).lower(), axis=1)
+
+# helper functions to split/sample data
+from warnings import warn
+
+def multilabel_sample(y, size=1000, min_count=5, seed=None):
+    """ Takes a matrix of binary labels `y` and returns
+        the indices for a sample of size `size` if
+        `size` > 1 or `size` * len(y) if size =< 1.
+        The sample is guaranteed to have > `min_count` of
+        each label.
+        
+        
+        Taken from pjbull and DataCamp, link provided above.
+    """
+    try:
+        if (np.unique(y).astype(int) != np.array([0, 1])).any():
+            raise ValueError()
+    except (TypeError, ValueError):
+        raise ValueError('multilabel_sample only works with binary indicator matrices')
+
+    if (y.sum(axis=0) < min_count).any():
+        raise ValueError('Some classes do not have enough examples. Change min_count if necessary.')
+
+    if size <= 1:
+        size = np.floor(y.shape[0] * size)
+
+    if y.shape[1] * min_count > size:
+        msg = "Size less than number of columns * min_count, returning {} items instead of {}."
+        warn(msg.format(y.shape[1] * min_count, size))
+        size = y.shape[1] * min_count
+
+    rng = np.random.RandomState(seed if seed is not None else np.random.randint(1))
+
+    if isinstance(y, pd.DataFrame):
+        choices = y.index
+        y = y.values
+    else:
+        choices = np.arange(y.shape[0])
+
+    sample_idxs = np.array([], dtype=choices.dtype)
+
+    # first, guarantee > min_count of each label
+    for j in range(y.shape[1]):
+        label_choices = choices[y[:, j] == 1]
+        label_idxs_sampled = rng.choice(label_choices, size=min_count, replace=False)
+        sample_idxs = np.concatenate([label_idxs_sampled, sample_idxs])
+
+    sample_idxs = np.unique(sample_idxs)
+
+    # now that we have at least min_count of each, we can just random sample
+    sample_count = int(size - sample_idxs.shape[0])
+
+    # get sample_count indices from remaining choices
+    remaining_choices = np.setdiff1d(choices, sample_idxs)
+    remaining_sampled = rng.choice(remaining_choices,
+                                   size=sample_count,
+                                   replace=False)
+
+    return np.concatenate([sample_idxs, remaining_sampled])
+
+def multilabel_sample_dataframe(df, labels, size, min_count=5, seed=None):
+    """ Takes a dataframe `df` and returns a sample of size `size` where all
+        classes in the binary matrix `labels` are represented at
+        least `min_count` times.
+    """
+    idxs = multilabel_sample(labels, size=size, min_count=min_count, 
+                             seed=seed)
+    return df.loc[idxs]
+
+
+
+def multilabel_train_test_split(X, Y, size, min_count=5, seed=None):
+    """ Takes a features matrix `X` and a label matrix `Y` and
+        returns (X_train, X_test, Y_train, Y_test) where all
+        classes in Y are represented at least `min_count` times.
+    """
+    index = Y.index if isinstance(Y, pd.DataFrame) else np.arange(Y.shape[0])
+
+    test_set_idxs = multilabel_sample(Y, size=size, min_count=min_count, 
+                                      seed=seed)
+    train_set_idxs = np.setdiff1d(index, test_set_idxs)
+
+    test_set_mask = index.isin(test_set_idxs)
+    train_set_mask = ~test_set_mask
+
+    return (X[train_set_mask], X[test_set_mask], Y[train_set_mask], Y[test_set_mask])
+
+
+from itertools import combinations
+from scipy import sparse
+from sklearn.base import BaseEstimator, TransformerMixin
+
+# this class will allow us to create interactions with a sparse matrix
+
+class SparseInteractions(BaseEstimator, TransformerMixin):
+    """
+    Customer transformer to create interaction terms for sparse matrices.
+    Created by pjbull and DataCamp.
+    """
+    def __init__(self, degree=2, feature_name_separator="_"):
+        self.degree = degree
+        self.feature_name_separator = feature_name_separator
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        if not sparse.isspmatrix_csc(X):
+            X = sparse.csc_matrix(X)
+
+        if hasattr(X, "columns"):
+            self.orig_col_names = X.columns
+        else:
+            self.orig_col_names = np.array([str(i) for i in range(X.shape[1])])
+
+        spi = self._create_sparse_interactions(X)
+        return spi
+
+    def get_feature_names(self):
+        return self.feature_names
+
+    def _create_sparse_interactions(self, X):
+        out_mat = []
+        self.feature_names = self.orig_col_names.tolist()
+
+        for sub_degree in range(2, self.degree + 1):
+            for col_ixs in combinations(range(X.shape[1]), sub_degree):
+                # add name for new column
+                name = self.feature_name_separator.join(self.orig_col_names[list(col_ixs)])
+                self.feature_names.append(name)
+
+                # get column multiplications value
+                out = X[:, col_ixs[0]]
+                for j in col_ixs[1:]:
+                    out = out.multiply(X[:, j])
+
+                out_mat.append(out)
+
+        return sparse.hstack([X] + out_mat)
+
+
+# import FunctionTransformer
+from sklearn.preprocessing import FunctionTransformer
+
+# use combine_text_columns to grab text features and combine them
+get_text_data = FunctionTransformer(combine_text_columns, validate=False)
+
+# use a lambda function to select only numeric columns
+get_numeric_data = FunctionTransformer(lambda x: x[NUMERIC_COLS], validate=False)
